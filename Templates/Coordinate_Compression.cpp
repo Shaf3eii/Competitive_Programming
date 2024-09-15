63aaefc4b035589ebf0211abd1292e2267c015cb
ShaFeiii <elshafeim45@gmail.com>
2024-09-15 18:29:25 +0300

    Coordinate_Compression_Template

---
+++ Templates/Coordinate_Compression.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct coordinate_compression {
+private:
+    vector<long long> comp;
+    void compression(vector<long long> &v) {
+        sort(v.begin(), v.end());
+        v.erase(unique(v.begin(),v.end()), v.end());
+    }
+public:
+    coordinate_compression(vector<long long> &v) {
+        comp = v;
+        compression(comp);
+    }
+    int get_index(long long val) {
+        return lower_bound(comp.begin(), comp.end(),comp[val]) - comp.begin();
+    }
+    long long get_origin(int idx) {
+        return comp[idx];
+    }
+};
+
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
