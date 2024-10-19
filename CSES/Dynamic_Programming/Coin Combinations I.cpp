44d8830d557231a2437931d7fc2d53b5ae7a940b
ShaFeiii <elshafeim45@gmail.com>
2024-10-19 11:55:27 +0300

    Dp Solutions

---
+++ CSES/Dynamic_Programming/Coin Combinations I.cpp
@@ -0,0 +1,81 @@
+//      ﷽
+
+//       وَأَنْ لَيْسَ لِلْإِنْسَانِ إِلَّا مَا سَعَى
+
+//      ShaFeiii Here
+
+//      "If you don't fight, You can't win!"
+
+//#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+#define Wl3_ElNor       ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define i64             long long
+#define int             long long
+#define all(a)          a.begin(),a.end()
+#define all_r(a)        a.rbegin(),a.rend()
+#define vi              vector<int>
+#define ef              else if
+#define retv(x)         return void(cout << (x) << '\n')
+#define sp              " "
+#define SetPre(n, x)    cout << fixed << setprecision(n) << x << '\n'
+#define Mod             1'000'000'007
+#define INF             2'000'000'000
+#define sz(s)           (int)(s).size()
+
+//const int dx[] = {0, 0, 1, -1};
+//const int dy[] = {1, -1, 0, 0};
+//const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//const int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+//const double pi = 3.14159265358979323846264338327950;
+//const double EPS = -1e9;
+
+int n, x;
+const int N = 1e6 + 5;
+int dp[N];
+vi coins;
+
+int No_Of_Ways(int cur_sum) {
+    if(cur_sum > x)
+        return 0;
+    if(cur_sum == x)
+        return 1;
+    auto& ret = dp[cur_sum];
+    if(ret != -1)
+        return ret;
+    ret = 0;
+    for(int i = 0 ; i < n ; ++i)
+        ret += No_Of_Ways((cur_sum + coins[i]) % Mod) % Mod;
+    return ret;
+}
+
+void Erz3() {
+    cin >> n >> x;
+    for(int i = 0, y ; i < n ; ++i) {
+        cin >> y;
+        coins.push_back(y);
+    }
+    memset(dp, -1, sizeof dp);
+    cout << No_Of_Ways(0) % Mod;
+}
+
+signed main() {
+    Wl3_ElNor
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+//    cin >> t;
+    while (t--)
+        Erz3();
+
+    return 0;
+}
