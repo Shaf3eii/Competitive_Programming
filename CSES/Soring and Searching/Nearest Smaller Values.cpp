c8faf691880477dddd79e771c83c760260c8489d
ShaFeiii <elshafeim45@gmail.com>
2024-11-04 23:54:22 +0200

    CSES Sotring&Searching

---
+++ CSES/Soring and Searching/Nearest Smaller Values.cpp
@@ -0,0 +1,69 @@
+//      ﷽
+
+//       وَأَنْ لَيْسَ لِلْإِنْسَانِ إِلَّا مَا سَعَى
+
+//#include <stdio.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//#include <ext/pb_ds/assoc_container.hpp>
+//#include <ext/pb_ds/tree_policy.hpp>
+//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
+
+// 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
+#define Wl3_ElNor       ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
+#define i64             long long
+#define int             long long
+#define all(a)          a.begin(),a.end()
+#define all_r(a)        a.rbegin(),a.rend()
+#define vi              vector<int>
+#define ef              else if
+#define retv(x)         return void(cout << (x) << '\n')
+#define sp              " "
+#define SetPre(n, x)    cout << fixed << setprecision(n) << x << '\n'
+#define Mod             1'000'000'007
+#define INF             2'000'000'000
+#define sz(s)           (int)(s).size()
+#define debug(x)        cout << #x << ": " << (x) << "\n";
+#define EPS             1e-9
+#define PI              acos(-1)
+
+//const int dx[] = {0, 0, 1, -1};
+//const int dy[] = {1, -1, 0, 0};
+//const int dx[] = {0, 1, 1, 1, 0, -1, -1, -1};
+//const int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
+//const int N = 1e3 + 5;
+
+void Erz3() {
+    int n;
+    cin >> n;
+    vi elements(n);
+    for(auto& i : elements)
+        cin >> i;
+    stack<pair<int,int>> st; // element   pos
+    st.push({0, 0});
+    for(int i = 0 ; i < n ; ++i) {
+        while(st.top().first >= elements[i])
+            st.pop();
+        int element = elements[i];
+        elements[i] = st.top().second;
+        st.push({element, i + 1});
+    }
+    for(int i = 0 ; i < n ; ++i)
+        cout << elements[i] << " \n"[i == n - 1];
+}
+
+signed main() {
+    Wl3_ElNor
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+#endif
+    int t = 1;
+//    cin >> t;
+    while (t--)
+        Erz3();
+
+    return 0;
+}
