53bf88750ad84dd251c28aa0c18c5e7215a7bdc3
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2024-11-11 12:38:58 +0200

    Coordinate_Compression.cpp

--- Templates/Coordinate_Compression.cpp
+++ Templates/Coordinate_Compression.cpp
@@ -4,20 +4,20 @@ using namespace std;
 
 struct coordinate_compression {
 private:
-    vector<long long> comp;
-    void compression(vector<long long> &v) {
-        sort(v.begin(), v.end());
-        v.erase(unique(v.begin(),v.end()), v.end());
+    vector<int> comp;
+    void compress() {
+        sort(comp.begin(), comp.end());
+        comp.erase(unique(comp.begin(), comp.end()), comp.end());
     }
 public:
-    coordinate_compression(vector<long long> &v) {
+    coordinate_compression(vector<int> & v) {
         comp = v;
-        compression(comp);
+        compress();
     }
-    int get_index(long long val) {
+    int get_index(int val) {
         return lower_bound(comp.begin(), comp.end(), val) - comp.begin(); // if we need to make it 1-based index add + 1
     }
-    long long get_origin(int idx) {
+    int get_origin(int idx) {
         return comp[idx];
     }
 };
