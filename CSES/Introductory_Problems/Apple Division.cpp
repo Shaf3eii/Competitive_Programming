10d43c0eef331804f2fe8b55b875de90a45793d6
Mahmoud Salah Elshafei <154061765+Shaf3eii@users.noreply.github.com>
2024-12-13 14:35:59 +0200

    Update Apple Division.cpp

--- CSES/Introductory_Problems/Apple Division.cpp
+++ CSES/Introductory_Problems/Apple Division.cpp
@@ -2,25 +2,9 @@
 
 //       وَأَنْ لَيْسَ لِلْإِنْسَانِ إِلَّا مَا سَعَى
 
-//   "From the river to the sea, Palestine is Free"
-
-// Seek knowledge from the Cradle to the Grave.
-
-// Never Give up -- Life is too short to make mistakes and learn from them -- Learn from other's mistakes
-
-// Remember you were always a novice when you started so never be rude to some one
-
-// Aim Big and strive hard to get it
-
-// One has his own set of talents and anyone can do anything so never compare yourself to anybody
-
 #include <bits/stdc++.h>
 
 using namespace std;
-
-//#include <ext/pb_ds/assoc_container.hpp>
-//#include <ext/pb_ds/tree_policy.hpp>
-//typedef __gnu_pbds::tree<int, __gnu_pbds::null_type, less<int>, __gnu_pbds::rb_tree_tag, __gnu_pbds::tree_order_statistics_node_update> ordered_set;
 
 // 48-57 -> 0-9  65-90 -> A-Z 97-122 -> a-z
 #define Free_Palestine  ios::sync_with_stdio(0),cin.tie(0),cout.tie(0);
@@ -37,6 +21,30 @@ using namespace std;
 //const int dy[] = {1, 1, 0, -1, -1, -1, 0, 1};
 //const i64 mod = 1e9 + 7;
 
+
+// recursive solution
+int n;
+vector<int> nums;
+
+int getMinDifference(int curIdx, int curDif) {
+    if (curIdx == n) return abs(curDif);
+
+    int rightGroup = getMinDifference(curIdx + 1, nums[curIdx] + curDif);
+    int leftGroup = getMinDifference(curIdx + 1, nums[curIdx] - curDif);
+
+    return min(rightGroup, leftGroup);
+}
+
+void Erz3() {
+    cin >> n;
+    nums.resize(n);
+    for (auto& i : nums) cin >> i;
+
+    cout << getMinDifference(0, 0);
+}
+
+
+// bitmasks solution
 void solve() {
     int n;
     cin >> n;
